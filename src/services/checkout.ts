0f16d0800bfe378f58a10b0ea10d778eabf5a29c
Loic Wong <loic.wong@mercury.co.nz>
2022-04-09 23:19:32 +1200

    scan item and orchestrate the product order with realtime original price total and amount

--- src/services/checkout.ts
+++ src/services/checkout.ts
@@ -1,6 +1,6 @@
 import {
     ICheckOut,
-    IPricingRules,
+    IPricingRules, IProductOrder,
 } from "../models/pojo";
 import {IProductEntity, IProductPromotionRelation, IPromotionAlgorithmEntity} from "../models/entity";
 import {groupBy} from "../utils/utils";
@@ -10,6 +10,7 @@ export class Checkout implements ICheckOut {
     readonly productPromoDictionary: Record<string,
         Array<IProductPromotionRelation>>;
     readonly promoAlgorithmDictionary: Record<string, IPromotionAlgorithmEntity>;
+    private cart: Record<string, IProductOrder> = {};
 
     constructor({
                     productPromoRelations,
@@ -30,6 +31,42 @@ export class Checkout implements ICheckOut {
     }
 
     scan(productEntity: IProductEntity): void {
+
+        const {sku, price} = productEntity;
+        const productOrder = this.cart[sku];
+
+        if (!productOrder) {
+            const promoAlgorithmDictionary = this.promoAlgorithmDictionary;
+            const productPromoRelationArray = this.productPromoDictionary[sku];
+
+            const isPromotionAvailable = () => productPromoRelationArray?.length > 0;
+            const getPromotionAlgorithm = (pid: string) =>
+                promoAlgorithmDictionary[pid];
+            const getPromotionAlgorithms = (pidList: string[]) =>
+                pidList.map((pid: string) => getPromotionAlgorithm(pid));
+            const getPromotionAlgorithmIds = () =>
+                productPromoRelationArray.map(
+                    (x: IProductPromotionRelation) => x.pid
+                );
+
+            const promotionAlgorithms = isPromotionAvailable()
+                ? getPromotionAlgorithms(getPromotionAlgorithmIds())
+                : undefined;
+            this.cart[sku] = {
+                ...productEntity,
+                amount: 1,
+                total: price,
+                promotionAlgorithms,
+            };
+            return;
+        }
+
+        const {amount, total}: IProductOrder = productOrder;
+        this.cart[sku] = {
+            ...productOrder,
+            amount: amount + 1,
+            total: total + price,
+        };
     }
 
     total(): number {
